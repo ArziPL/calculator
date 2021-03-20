7246c6828d509d091160df318f0aedcce9e44b05
Arek <arrek1116@wp.pl>
2021-03-20 23:28:56 +0100

    First JS logic

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const resultBlock = document.querySelector(".calc__result");
-const result = document.querySelector(".calc__operations").textContent;
+const result = document.querySelector(".calc__operations");
 
 
 // COPY RESULT TO CLIPBOARD
@@ -10,3 +10,16 @@ function copyToClipboard() {
     navigator.clipboard.writeText(result);
 }
 
+// CALCULATOR FUNCTION
+
+let temporaryValue = "";
+
+function calculations(val) {
+    if(/[123456789]/g.test(val)) {
+        temporaryValue += val;
+        result.textContent = temporaryValue;
+    }
+}
+
+calculations("2");
+calculations("5")
