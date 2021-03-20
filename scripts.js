c2b02eefdb59d4e7574bf2a27b671ebd325bec51
Arek <arrek1116@wp.pl>
2021-03-20 23:43:56 +0100

    Better basic functions

--- scripts.js
+++ scripts.js
@@ -15,11 +15,48 @@ function copyToClipboard() {
 let temporaryValue = "";
 
 function calculations(val) {
-    if(/[123456789]/g.test(val)) {
+
+    if(/[123456789,]/g.test(val)) {
+        if(temporaryValue.includes(",") && val == ","){
+            return
+        }
         temporaryValue += val;
         result.textContent = temporaryValue;
     }
+
+
+    if(val == "C") {
+        result.textContent = result.textContent.slice(0,-1);
+        temporaryValue = temporaryValue.slice(0,-1);
+        if(result.textContent == "") {
+            result.textContent = "0";
+        }
+    }
+
+    if(val == "CE") {
+        result.textContent = "0";
+        temporaryValue = "";
+        return
+    }
 }
 
+// if(/[+-*/^&]/)
+// }
+
 calculations("2");
-calculations("5")
+calculations("5");
+calculations(",");
+calculations("5");
+calculations(",");
+calculations(",");
+calculations("5");
+calculations("CE");
+calculations("2");
+calculations("5");
+calculations("5");
+calculations("C");
+calculations("5");
+calculations("2");
+
+
+
