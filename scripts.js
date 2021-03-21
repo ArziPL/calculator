77aed4a36a09a0f1fa2cb3b5d5ff16dc1740bb00
Arek <arrek1116@wp.pl>
2021-03-21 02:05:02 +0100

    Not clickable, but working calc

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 const resultBlock = document.querySelector(".calc__result");
 const result = document.querySelector(".calc__operations");
+let inputBtnList = document.querySelectorAll(".calc__card");
+for(ele of inputBtnList) {
+    ele.addEventListener("click", () => {
+        calculations(ele.value);
+    });
+}
+
+
+
+
+
+function addListnerHandler() {
+   
+}
+
+
+
+
 
 
 // COPY RESULT TO CLIPBOARD
@@ -7,56 +25,129 @@ const result = document.querySelector(".calc__operations");
 resultBlock.addEventListener("click", copyToClipboard);
 
 function copyToClipboard() {
-    navigator.clipboard.writeText(result);
+    navigator.clipboard.writeText(result.textContent);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // CALCULATOR FUNCTION
 
-let temporaryValue = "";
+
+let temporaryValueFirst = "";
+let temporaryValueSec = "";
+let temporarySymbol = "";
 
 function calculations(val) {
 
     if(/[123456789,]/g.test(val)) {
-        if(temporaryValue.includes(",") && val == ","){
+        if(temporarySymbol != "") {
+            if(temporaryValueSec.includes(",") && val == ","){
+                return
+            }
+            temporaryValueSec += val;
+            result.textContent = temporaryValueSec;
             return
         }
-        temporaryValue += val;
-        result.textContent = temporaryValue;
+        if(temporaryValueFirst.includes(",") && val == ","){
+            return
+        }
+        temporaryValueFirst += val;
+        result.textContent = temporaryValueFirst;
     }
 
 
     if(val == "C") {
+        if(temporarySymbol != "") {
+            result.textContent = result.textContent.slice(0,-1);
+            temporaryValueSec = temporaryValueSec.slice(0,-1);
+            if(result.textContent == "") {
+               result.textContent = "0";
+               temporaryValueFirst = "";
+               temporaryValueSec = "";
+               temporarySymbol = "";
+        }
+        return
+        }
         result.textContent = result.textContent.slice(0,-1);
         temporaryValue = temporaryValue.slice(0,-1);
         if(result.textContent == "") {
             result.textContent = "0";
+
         }
+        return
     }
 
     if(val == "CE") {
         result.textContent = "0";
-        temporaryValue = "";
-        return
+        temporaryValueFirst = "";
+        temporarySymbol = "";
+        temporaryValueSec = ""
     }
+
+    if(/[\^&/*+-]/g.test(val)) {
+        temporarySymbol = val;
+    }
+
+    if(temporaryValueFirst != "" && temporarySymbol != "" && temporaryValueSec != "" && val == "=") {
+        switch(temporarySymbol) {
+            case "+":
+                temporaryValueFirst = result.textContent = parseFloat(temporaryValueFirst) + parseFloat(temporaryValueSec);
+                temporarySymbol = "";
+                temporaryValueSec = ""
+                break;
+            case "-":
+                temporaryValueFirst = result.textContent = parseFloat(temporaryValueFirst) - parseFloat(temporaryValueSec);
+                temporarySymbol = "";
+                temporaryValueSec = ""
+                break;
+            case "*":
+                temporaryValueFirst = result.textContent = parseFloat(temporaryValueFirst) * parseFloat(temporaryValueSec);
+                temporarySymbol = "";
+                temporaryValueSec = ""
+                break;
+            case "/":
+                temporaryValueFirst = result.textContent = parseFloat(temporaryValueFirst) / parseFloat(temporaryValueSec);
+                temporarySymbol = "";
+                temporaryValueSec = ""
+                break;
+            case "^":
+                temporaryValueFirst = result.textContent = Math.pow(parseFloat(temporaryValueFirst),parseFloat(temporaryValueSec));
+                temporarySymbol = "";
+                temporaryValueSec = ""
+                break;
+
+        }
+    }
+
+    if(val == "&") {
+        temporaryValueFirst = result.textContent = Math.sqrt(parseFloat(temporaryValueFirst));
+        temporarySymbol = "";
+        temporaryValueSec = "";
+    }
+
 }
 
-// if(/[+-*/^&]/)
-// }
 
-calculations("2");
-calculations("5");
-calculations(",");
-calculations("5");
-calculations(",");
-calculations(",");
-calculations("5");
-calculations("CE");
-calculations("2");
-calculations("5");
-calculations("5");
-calculations("C");
-calculations("5");
-calculations("2");
+sqrt\(\)|[\^$/*+-]
+
+
+
+
+
+
+
 
 
 
